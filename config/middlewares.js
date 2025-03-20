50bd2ec9920a4ab3b310bbc1d3757531ea080f36
Omar Hossam Eldin Kandil <62789045+OmarHossamEldin@users.noreply.github.com>
2025-03-20 10:27:50 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,37 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'http:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'http:', 'https:'],
+          'script-src': ["'self'", 'http:', 'https:'],
+          'frame-src': ["'self'", 'http:', 'https:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: '*', //change it to frontend domainname in production
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Access-Control-Allow-Origin',
+        'Accept-Language',
+        'Accept-Encoding',
+        'Accept'
+      ],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
